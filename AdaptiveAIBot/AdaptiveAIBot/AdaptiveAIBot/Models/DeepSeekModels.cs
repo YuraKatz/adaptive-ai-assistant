5af71daa9eda1c0b87db772dd3c75ce7fbcea535
YuraKatz <yurakatz@gmail.com>
2025-08-09 06:58:42 +0300

    ref

--- AdaptiveAIBot/AdaptiveAIBot/AdaptiveAIBot/Models/DeepSeekModels.cs
+++ AdaptiveAIBot/AdaptiveAIBot/AdaptiveAIBot/Models/DeepSeekModels.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace AdaptiveAIBot.Models
 {
-    internal class DeepSeekModels
+    public class DeepSeekRequest
     {
+        [JsonPropertyName("model")]
+        public string Model { get; set; } = string.Empty;
+
+        [JsonPropertyName("messages")]
+        public DeepSeekMessage[] Messages { get; set; } = Array.Empty<DeepSeekMessage>();
+
+        [JsonPropertyName("max_tokens")]
+        public int MaxTokens { get; set; }
+
+        [JsonPropertyName("temperature")]
+        public double Temperature { get; set; }
+    }
+
+    public class DeepSeekResponse
+    {
+        [JsonPropertyName("choices")]
+        public DeepSeekChoice[]? Choices { get; set; }
+    }
+
+    public class DeepSeekChoice
+    {
+        [JsonPropertyName("message")]
+        public DeepSeekMessage? Message { get; set; }
+    }
+
+    public class DeepSeekMessage
+    {
+        [JsonPropertyName("role")]
+        public string Role { get; set; } = string.Empty;
+
+        [JsonPropertyName("content")]
+        public string? Content { get; set; }
     }
 }
--- AdaptiveAIBot/AdaptiveAIBot/AdaptiveAIBot/Models/TelegramModels.cs
+++ AdaptiveAIBot/AdaptiveAIBot/AdaptiveAIBot/Models/TelegramModels.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace AdaptiveAIBot.Models
 {
-    internal class TelegramModels
+    public class TelegramUpdate
     {
+        [JsonPropertyName("update_id")]
+        public int UpdateId { get; set; }
+
+        [JsonPropertyName("message")]
+        public TelegramMessage? Message { get; set; }
+    }
+
+    public class TelegramMessage
+    {
+        [JsonPropertyName("message_id")]
+        public int MessageId { get; set; }
+
+        [JsonPropertyName("from")]
+        public TelegramUser? From { get; set; }
+
+        [JsonPropertyName("chat")]
+        public TelegramChat Chat { get; set; } = null!;
+
+        [JsonPropertyName("text")]
+        public string? Text { get; set; }
+    }
+
+    public class TelegramUser
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string? FirstName { get; set; }
+
+        [JsonPropertyName("username")]
+        public string? Username { get; set; }
+    }
+
+    public class TelegramChat
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
     }
 }
